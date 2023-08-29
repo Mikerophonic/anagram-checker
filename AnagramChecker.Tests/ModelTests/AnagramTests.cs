cf40210f7dd65885b7ff7cdde8449fa315d54129
Mikerophonic <micgcon@gmail.com>
2023-08-28 20:20:22 -0700

    pass test 3, create list

--- AnagramChecker.Tests/ModelTests/AnagramTests.cs
+++ AnagramChecker.Tests/ModelTests/AnagramTests.cs
@@ -11,8 +11,23 @@ namespace AnagramChecker.TestTools
         [TestMethod]
         public void AnagramConstructor_CreatesInstanceOfAnagram_Anagram()
         {
-          Anagram newAnagram = new Anagram();
+          Anagram newAnagram = new Anagram("bread");
           Assert.AreEqual(typeof(Anagram), newAnagram.GetType());
+        }
+        [TestMethod]
+        public void GetAnagramWord_ReturnsAnagramWord_String()
+        {
+          string anagramWord = "bread";
+          Anagram newAnagram = new Anagram(anagramWord);
+          string result = newAnagram.AnagramWord;
+          Assert.AreEqual(anagramWord, result);
+        }
+        [TestMethod]
+        public void MakeList_CreatesInstanceofList_List()
+        {
+          List<string> expected = new List<string> { "beard" };
+          Assert.AreEqual(typeof(List<string>), expected.GetType());
         }
     }
 }
+
