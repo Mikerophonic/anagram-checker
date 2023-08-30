fd32900119bba0d5ef30a6c303c9d48910dd535f
Mikerophonic <micgcon@gmail.com>
2023-08-29 20:23:22 -0700

    change test 3, pass, make word list array

--- AnagramChecker.Tests/ModelTests/AnagramTests.cs
+++ AnagramChecker.Tests/ModelTests/AnagramTests.cs
@@ -3,7 +3,7 @@ using AnagramChecker.Models;
 using System.Collections.Generic;
 using System;
 
-namespace AnagramChecker.TestTools
+namespace AnagramChecker.Test
 {
     [TestClass]
     public class AnagramTests
@@ -22,20 +22,25 @@ namespace AnagramChecker.TestTools
           string result = newAnagram.AnagramWord;
           Assert.AreEqual(anagramWord, result);
         }
+
         [TestMethod]
-        public void MakeList_CreatesInstanceofList_List()
+        public void GetWordListArray_CreateAStringArrayFromUserInpuStringArray() 
         {
-          List<string> expected = new List<string> { "beard" };
-          Assert.AreEqual(typeof(List<string>), expected.GetType());
+          string anagramWord = "bread";
+          Anagram newAnagram = new Anagram(anagramWord);
+          string userWordList = "bread beard butter bear pickle";
+          string[] userWordListArray = newAnagram.GetWordListArray(userWordList);
+          Assert.AreEqual(typeof(string[]), userWordListArray.GetType());
         }
+
+
         [TestMethod]
         public void GetArray_CreatesArrayOfAnagramWord_Array()
         {
           string anagramWord = "bread";
-          char[] anagramArray = GetArray(anagramWord);
-          Assert.AreEqual(typeof(Array), anagramArray.GetType());
-
-          
+          Anagram newAnagram = new Anagram(anagramWord);
+          char[] anagramArray = newAnagram.GetArray(anagramWord);
+          Assert.AreEqual(typeof(char[]), anagramArray.GetType());
         }
     }
 }
